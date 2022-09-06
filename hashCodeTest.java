1f2022b45439cbaa2affd0919e872af3eed9f1fd
Franklin, Scott Reagan <sfranklin36@gatech.edu>
2022-09-06 09:19:54 -0500

    Create hashCodeTest.java

---
+++ hashCodeTest.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class hashCodeTest {
+    public static void main(String[] args) {
+        String s1 = "Siblings";
+        String s2 = "Teheran";
+
+        StdOut.println(s1.hashCode());
+        StdOut.println(s2.hashCode());
+    }
+}
